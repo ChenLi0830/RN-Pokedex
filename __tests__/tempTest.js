73ba550a80c7ea2a72919bb4dd89d7c408903903
Chen Li <yichen.li0830@gmail.com>
2016-08-24 22:43:48 -0230

    Add deep-freeze library in the test for to ensure the functions are pure function (doesn't change original argument)

--- __tests__/tempTest.js
+++ __tests__/tempTest.js
@@ -1,12 +1,14 @@
 /**
  * Created by Chen on 2016-08-24.
  */
-
+import deepFreeze from 'deep-freeze';
 const addCounter = (list)=> [...list, 0];
+
 const removeCounter = (list, index)=> [
     ...list.slice(0, index),
     ...list.slice(index+1)
 ];
+
 const incrementCounter = (list,index) => [
     ...list.slice(0, index),
     list[index]+1,
@@ -18,6 +20,7 @@ describe("AddCounter", ()=>{
     let prevList = [10,20,1],
         newList = [10,20,1,0];
     
+    deepFreeze(prevList);
     expect(
         addCounter(prevList)
     ).toEqual(newList);
@@ -28,14 +31,21 @@ describe("removeCounter", ()=>{
   it("remove the counter at index i from a given list", ()=>{
     let prevList = [10,20,30,40],
         newList = [10,30,40];
-    
-    expect(removeCounter(prevList, 1)).toEqual(newList);
+  
+    deepFreeze(prevList);
+    expect(
+        removeCounter(prevList, 1)
+    ).toEqual(newList);
   })
 });
 
 describe("incrementCounter", ()=>{
   it("increase the counter at index for the list", ()=>{
     let prevList = [1,2,3], newList = [1,3,3];
-    expect(incrementCounter(prevList, 1)).toEqual(newList);
+    
+    deepFreeze(prevList);
+    expect(
+        incrementCounter(prevList, 1)
+    ).toEqual(newList);
   })
 });
